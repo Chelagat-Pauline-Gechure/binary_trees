c52bb249b5010ea966738510843df7e2b20c571b
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-08-28 11:22:07 +0300

    Corrected more errors

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,10 +13,10 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
     binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
-    
-    if (new_node == NULL) 
+
+    if (new_node == NULL)
     {
-        return NULL; // Return NULL on failure to allocate memory
+        return (NULL);
     }
 
     new_node->parent = parent;
