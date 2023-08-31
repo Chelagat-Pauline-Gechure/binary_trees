fbdf5c370ffbcc749d463fe0fc8361c262b23a97
gkiplangat <gideonkiplangat4@gmail.com>
2023-08-31 13:14:28 -0700

    9. Height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,18 +6,17 @@
  * @height: The current height of the tree being traversed
  * @max: A pointer to the maximum height found so far
  * Return:  the height
- *
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int right_counter = 0, left_counter = 0;
+	int counter_right = 0, counter_left = 0;
 
 	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
-	left_counter = binary_tree_height(tree->left);
-	right_counter = binary_tree_height(tree->right);
-	if (left_counter > right_counter)
-		return (left_counter + 1);
+	counter_left = binary_tree_height(tree->left);
+	counter_right = binary_tree_height(tree->right);
+	if (counter_left > counter_right)
+		return (counter_left + 1);
 	else
-		return (right_counter + 1);
+		return (counter_right + 1);
 }
